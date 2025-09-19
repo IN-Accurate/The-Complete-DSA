bbf70b1846f5216b0a9bff364310053f720c625c
IN-Accurate <vishalsankar410@gmail.com>
2025-09-20 02:15:10 +0530

    solve string problem 4

---
+++ Strings/4-removing-consecutive-characters.py
@@ -0,0 +1,13 @@
+def main():
+    s = input("Enter the string: ")
+    ans=""
+    ans +=s[0]
+    prev=s[0]
+    for i in range (1,len(s)):
+            if(s[i]!=prev):
+                 ans+=s[i]
+                 prev=s[i]
+            i +=1
+    print("New string: ",ans)
+if __name__ == '__main__':
+    main()
